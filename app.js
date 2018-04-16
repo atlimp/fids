aa9a371c54994bd0a4627178885dacab616ef802
Atli Marcher <amp17@hi.is>
2018-04-16 10:34:22 +0000

    error catching

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const {
-  getDepartures,
-  getArrivals,
-  getAirlines,
-  getStats,
-} = require('./fetch');
+const flights = require('./flights');
 
 const app = express();
 
-const {
-  PORT: port = 3000,
-  HOST: host = '127.0.0.1',
-} = process.env;
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -23,39 +14,23 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('/', flights);
 
-app.get('/arrivals/', async (req, res) => {
-  const arrivals = await getArrivals();
-  res.status(200).json(arrivals);
-});
+function notFoundHandler(req, res, next) {
+  return res.status(404).json({ error: '404-Not Found' });
+}
 
-app.get('/arrivals/:airline', async (req, res) => {
-  const { airline } = req.params;
-  const arrivals = await getArrivals(airline);
-  res.status(200).json(arrivals);
-});
+function errorHandler(err, res, res, next) {
+  return res.status(500).json({ error: '500-Internal Server Error'});
+}
 
-app.get('/departures/', async (req, res) => {
-  const arrivals = await getArrivals();
-  res.status(200).json(arrivals);
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-app.get('/departures/:airline', async (req, res) => {
-  const { airline } = req.params;
-  const arrivals = await getArrivals(airline);
-  res.status(200).json(arrivals);
-});
-
-app.get('/airlines', async (req, res) => {
-  const airlines = await getAirlines();
-  res.status(200).json(airlines);
-});
-
-app.get('/stats', async (req, res) => {
-  const stats = await getStats();
-  res.status(200).json(stats);
-});
-
+const {
+  PORT: port = 3000,
+  HOST: host = '127.0.0.1',
+} = process.env;
 app.listen(port, () => {
   console.info(`Server running http://${host}:${port}`);
 });
---
+++ flights.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const {
+  getDepartures,
+  getArrivals,
+  getAirlines,
+  getStats,
+} = require('./fetch');
+
+function catchErrors(fn) {
+  return (req, res, next) => fn(req, res, next).catch(next);
+}
+
+const router = express.Router();
+
+async function arrivalRoute(req, res) {
+  const arrivals = await getArrivals();
+  res.status(200).json(arrivals);
+}
+
+async function arrivalAirlineRoute(req, res) {
+  const { airline } = req.params;
+  const arrivals = await getArrivals(airline);
+  res.status(200).json(arrivals);
+}
+
+async function departureRoute(req, res) {
+  const arrivals = await getArrivals();
+  res.status(200).json(arrivals);
+}
+
+async function departureAirlineRoute(req, res) {
+  const { airline } = req.params;
+  const arrivals = await getArrivals(airline);
+  res.status(200).json(arrivals);
+}
+
+async function airlineRoute(req, res) {
+  const airlines = await getAirlines();
+  res.status(200).json(airlines);
+}
+
+async function statsRoute(req, res) {
+  const stats = await getStats();
+  res.status(200).json(stats);
+}
+
+router.get('/arrivals', catchErrors(arrivalRoute));
+router.get('/arrivals/:airline', catchErrors(arrivalAirlineRoute));
+router.get('/departures', catchErrors(departureRoute));
+router.get('/departures/:airline', catchErrors(departureAirlineRoute));
+router.get('/airlines', catchErrors(airlineRoute));
+router.get('/stats', catchErrors(statsRoute));
+
+module.exports = router;
