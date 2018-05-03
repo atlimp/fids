5cdd98225b46be92fee33c2614ebfcae6bed885a
amp17 <amp17@github.com>
2018-05-03 13:23:17 +0000

    bæta við - á header

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ window.onload = () => {
   const headers = document.querySelectorAll('.header');
   headers.forEach(header => {
     header.addEventListener('click', () => {
+      header.classList.toggle('minimized');
       const tbody = header.nextSibling.querySelector('tbody');
       tbody.classList.toggle('collapsed');
     });
